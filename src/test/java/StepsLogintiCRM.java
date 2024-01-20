da3196966ca66038d2681ad777e8c44fc3008f9b
Trang Le <trangle.uit@gmail.com>
2024-01-20 13:26:05 +0700

    innitial commit

---
+++ src/test/java/StepsLogintiCRM.java
@@ -0,0 +1,26 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepsLogintiCRM {
+    @Given("I am on the login page")
+    public void iAmOnTheLoginPage() {
+    }
+
+    @When("I enter my username and password")
+    public void iEnterMyUsernameAndPassword() {
+    }
+
+    @And("I click the Login button")
+    public void iClickTheLoginButton() {
+    }
+
+    @Then("I should be taken to the Dashboard page")
+    public void iShouldBeTakenToTheDashboardPage() {
+    }
+
+    @And("I should see the {string} menu")
+    public void iShouldSeeTheMenu(String arg0) {
+    }
+}
